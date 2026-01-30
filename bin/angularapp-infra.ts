7bde7f4efbf0a7de6f4171d21966d7824c070e4c
vicente-jpro <vicenteviciii@outlook.com>
2026-01-30 16:12:43 +0100

    feat: Helloword stack added.

--- bin/angularapp-infra.ts
+++ bin/angularapp-infra.ts
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib/core';
 import { AmplifyHostingStack } from '../lib/amplify-stack';
+import { HellowordStack } from '../lib/helloword-stack';
 
 const app = new cdk.App();
 
 const amplifyStack = new AmplifyHostingStack(app, 'AngularAppAmplifyStack', {});
+
+const hellowordStack = new HellowordStack(app, 'HellowordStack', {});
---
+++ lib/helloword-stack.ts
@@ -0,0 +1,52 @@
+import { CfnOutput, SecretValue, Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+
+import { 
+    App, 
+    GitHubSourceCodeProvider,
+    RedirectStatus
+} from "@aws-cdk/aws-amplify-alpha";
+
+
+interface HellowordStackProps extends StackProps {
+
+}
+
+export class HellowordStack extends Stack {
+    constructor(scope: Construct, id: string, props?: HellowordStackProps) {
+        super(scope, id, props);
+
+        // Create the Amplify application 
+        const amplifyApp = new App(this, `HellowordApp`, {
+            sourceCodeProvider: new GitHubSourceCodeProvider({
+                owner: 'vicente-jpro',
+                repository: 'helloword',
+                oauthToken: SecretValue.secretsManager('GITHUB_ACCESS_TOKEN2'),
+        }), 
+        environmentVariables: {
+            REGION: this.region,
+        },
+        });
+
+        // Add a branch to the Amplify application
+        const main = amplifyApp.addBranch('main',{
+            autoBuild: true,
+            stage: 'PRODUCTION'
+        });
+
+        amplifyApp.addCustomRule({
+            source: '</^[^.]+$|\\.(?!(css|gif|ico|jpg|js|png|txt|svg|woff|woff2|eot|ttf|map)$)([^.]+$)/>',
+            target: '/index.html',
+            status: RedirectStatus.REWRITE
+        });
+
+
+        new CfnOutput(this, 'HellowordAppName', {
+            value: amplifyApp.appName
+        });
+
+        new CfnOutput(this, 'HellowordAppUrl', {
+            value: `http://main.${amplifyApp.defaultDomain}`
+        });
+    }
+}
